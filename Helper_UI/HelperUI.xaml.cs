64668f9e98d37f693e1b7a116d9e66b456f2e9bf
Aravind Kothandaraman <9501793@carmax.com>
2019-03-05 12:17:50 -0500

    Clean ups

--- Helper_UI/HelperUI.xaml.cs
+++ Helper_UI/HelperUI.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using RunMyQuery;
+﻿using System.Windows;
 
 namespace Helper_UI
 {
@@ -21,8 +8,6 @@ namespace Helper_UI
     public partial class HelperUI : Window
     {
         SplitAndMerge splitMergeWindow;
-        RunMyQuery.MainWindow runQueryWindow;
-        AgentView agtView;
 
         public HelperUI()
         {
@@ -42,23 +27,5 @@ namespace Helper_UI
 
             splitMergeWindow.Show();
         }
-
-        private void BtnRunMyQuery_Click(object sender, RoutedEventArgs e)
-        {
-            if (runQueryWindow == null)
-                runQueryWindow = new RunMyQuery.MainWindow();
-
-            runQueryWindow.Show();
-        }
-
-        private void BtnAgentView_Click(object sender, RoutedEventArgs e)
-        {
-            if (agtView == null)
-                agtView = new AgentView();
-
-            agtView.Show();
-        }
-
-
     }
 }
