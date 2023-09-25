f62ce917cb385734250e8b7789d0cbd9353528b0
Aravind Kothandaraman <aravind.k@spglobal.com>
2023-09-25 13:46:14 -0400

    Added encryptor flow

---
+++ Helper_UI/SplitMerge/SplitMergeViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Helper_UI.SplitMerge
+{
+    public class SplitMergeViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ICommand firstCmd;
+        private ICommand prevCmd;
+        private ICommand nextCmd;
+        private ICommand LastCmd;
+
+        #region Commands
+        public ICommand FirstPageCommand
+        {
+            get
+            {
+                if (firstCmd == null)
+                {
+                //    firstCmd = new RelayCommand(
+                //    param =>
+                //    {
+                //        start = 0;
+                //        RefreshProducts();
+
+                //    },
+                //    param =>
+                //    {
+                //        return start - itemCount >= 0 ? true : false;
+                //    }
+                //);
+                //firstCmd = new RelayCommand(param =>)
+                }
+                return firstCmd;
+            }
+        }
+        #endregion
+    }
+}
